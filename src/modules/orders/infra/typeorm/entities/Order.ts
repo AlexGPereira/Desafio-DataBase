05eb244dbe8a006112f5ff70a83bef0d3629aefe
AlexGPereira <alexsander.gimente@hotmail.com>
2021-02-28 12:21:54 -0300

    Fim

---
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+} from 'typeorm';
+
+import Customer from '@modules/customers/infra/typeorm/entities/Customer';
+import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
+
+@Entity('orders')
+class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: 'customer_id' }) // para não precisar criar coluna customer_id
+  customer: Customer;
+
+  /*
+  estamos criando um relacionamento many to many, mas como usamos um custom pivô
+  criamos um onetomany numa ponta e um manytoone na outra ponta e isto forma uma manytomany customizado
+  */
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true,
+  })
+  order_products: OrdersProducts[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Order;
